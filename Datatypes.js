9088a8ac6855dde53f1ea939cca22eab3b6aa1b7
Vaishali <vaishalirawat1119@gmail.com>
2024-08-22 23:01:22 +0530

    Some basic datatypes

---
+++ Datatypes.js
@@ -0,0 +1,19 @@
+// it is a string datatype
+let name = "John"
+
+// it is a number datatype
+let age = 18
+
+// it is a boolean datatype , it can  be true or false
+let isLoggedIn = false
+
+// it is a undefined variable
+let state;
+
+// it is a null datatype, which means that we have a variable named "marks" but it doesn't contain any value so that's why it is a null
+let marks = null
+
+
+// Typeof is a keyword which returns the type of variable
+console.log(typeof null);
+console.log(typeof undefined);
