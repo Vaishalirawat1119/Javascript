ef27560c35eb23159c4ca7c1d6d8dd4de7ffa079
Vaishali <vaishalirawat1119@gmail.com>
2024-08-23 21:56:54 +0530

    Some basic operations

---
+++ Operations.js
@@ -0,0 +1,28 @@
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+let str1 = "hello ";
+let str2 = "John ";
+let str3 = str2+str1;
+
+// console.log(str3);
+
+console.log( "1" + 2);          // -> 12
+console.log(1 + "2");           // -> 12
+console.log("1" + "2");         // -> 12
+console.log("1" + 2 + 2);       // -> 122
+console.log(1 + 2 + "2");       // -> 32
+console.log("1" + "2" + "2");   // -> 122
+console.log(1 + "2" + 2);       // -> 122
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
