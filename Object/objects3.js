8ad790e1a9778bbda2d67b2fbe67159a21828165
Vaishalirawat1119 <112968518+Vaishalirawat1119@users.noreply.github.com>
2025-01-02 22:08:52 +0530

    Object de-structuring

---
+++ Object/objects3.js
@@ -0,0 +1,15 @@
+const Car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2020,
+    color: 'blue',
+    price: 1500000,
+    engine: 'V6',
+}
+
+// const details = Car.price;
+// console.log(details);
+
+const {color:clr} = Car;
+console.log(clr);
+
